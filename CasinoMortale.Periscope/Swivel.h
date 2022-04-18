861084ef8c04d6830b51fe70effb8b267c49d5db
Bas Paap <baspaap@live.nl>
2022-04-18 20:19:14 +0200

    Change servo pulse widths.

--- CasinoMortale.Periscope/Swivel.h
+++ CasinoMortale.Periscope/Swivel.h
@@ -15,8 +15,8 @@ namespace CasinoMortale
 {
 	class Swivel
 	{	
-		const int minPulseWidth = 720;
-		const int maxPulseWidth = 2400;
+		const int minPulseWidth = 450;
+		const int maxPulseWidth = 2450;
 		const float speed = 200.0f;
 		Servo servo;
 		int servoPin;
